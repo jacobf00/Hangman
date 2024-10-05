8adb196697e8259fae8fc8426704b6661007696f
jacobf00 <jfoulds00@gmail.com>
2024-10-05 19:41:22 -0400

    remove namespace indent

--- GameState.cs
+++ GameState.cs
@@ -1,94 +1,92 @@
 using System.Dynamic;
 
-namespace Hangman.State
+namespace Hangman.State;
+
+public enum HangmanState
 {
+    InProgress,
+    OutOfGuesses,
+    Won
+}
 
-    public enum HangmanState
+public enum GuessState
+{
+    GuessedCorrect,
+    GuessedWrong,
+    GuessedRepeated
+}
+class GameState
+{
+    public string SecretWord { get; set; }
+    public char[] WordState { get; set; }
+    public char[] Guesses { get; set; }
+    public HangmanState State { get; set; }
+    public GuessState LastGuessState { get; set; }
+
+
+    public GameState(string secret_word)
     {
-        InProgress,
-        OutOfGuesses,
-        Won
+        SecretWord = secret_word;
+        WordState = Enumerable.Repeat('*', secret_word.Length).ToArray();
+        Guesses = new char[7];
+        State = HangmanState.InProgress;
     }
 
-    public enum GuessState
+    public int GetNumGuesses()
     {
-        GuessedCorrect,
-        GuessedWrong,
-        GuessedRepeated
+        return Guesses.Count(c => c != '\0');
     }
-    class GameState
+
+    public bool Guess(char guess)
     {
-        public string SecretWord { get; set; }
-        public char[] WordState { get; set; }
-        public char[] Guesses { get; set; }
-        public HangmanState State { get; set; }
-        public GuessState LastGuessState { get; set; }
+        bool shouldContinue = true;
 
 
-        public GameState(string secret_word)
+        var correctlyGuessedIndices = SecretWord
+            .Select((c, index) => new { c, index })
+            .Where(x => x.c == guess)
+            .Select(x => x.index)
+            .ToList();
+
+        if (!correctlyGuessedIndices.Any())
         {
-            SecretWord = secret_word;
-            WordState = Enumerable.Repeat('*', secret_word.Length).ToArray();
-            Guesses = new char[7];
-            State = HangmanState.InProgress;
-        }
-
-        public int GetNumGuesses()
-        {
-            return Guesses.Count(c => c != '\0');
-        }
-
-        public bool Guess(char guess)
-        {
-            bool shouldContinue = true;
-
-
-            var correctlyGuessedIndices = SecretWord
-                .Select((c, index) => new { c, index })
-                .Where(x => x.c == guess)
-                .Select(x => x.index)
-                .ToList();
-
-            if (!correctlyGuessedIndices.Any())
+            if (Guesses.Contains(guess))
             {
-                if (Guesses.Contains(guess))
-                {
-                    LastGuessState = GuessState.GuessedRepeated;
-                    return shouldContinue;
-                }
-                LastGuessState = GuessState.GuessedWrong;
-                // if guess char isn't found in SecretWord, add guess to Guesses array
-                for (int i = 0; i < Guesses.Length; i++)
-                {
-                    if (Guesses[i] == '\0')
-                    {
-                        Guesses[i] = guess;
-                        break;
-                    }
-                }
-                // if adding the guess fills the Guesses array, set State to OutOfGuesses and stop the game loop
-                if (!Guesses.Contains('\0'))
-                {
-                    State = HangmanState.OutOfGuesses;
-                    shouldContinue = false;
-                }
+                LastGuessState = GuessState.GuessedRepeated;
                 return shouldContinue;
             }
-            else
+            LastGuessState = GuessState.GuessedWrong;
+            // if guess char isn't found in SecretWord, add guess to Guesses array
+            for (int i = 0; i < Guesses.Length; i++)
             {
-                LastGuessState = GuessState.GuessedCorrect;
-                foreach (int index in correctlyGuessedIndices)
+                if (Guesses[i] == '\0')
                 {
-                    WordState[index] = guess;
+                    Guesses[i] = guess;
+                    break;
                 }
-                // if adding the guess fills the WordState array, set State to Won and stop the game loop
-                if (!WordState.Contains('*'))
-                {
-                    State = HangmanState.Won;
-                    shouldContinue = false;
-                }
-                return shouldContinue;
             }
+            // if adding the guess fills the Guesses array, set State to OutOfGuesses and stop the game loop
+            if (!Guesses.Contains('\0'))
+            {
+                State = HangmanState.OutOfGuesses;
+                shouldContinue = false;
+            }
+            return shouldContinue;
+        }
+        else
+        {
+            LastGuessState = GuessState.GuessedCorrect;
+            foreach (int index in correctlyGuessedIndices)
+            {
+                WordState[index] = guess;
+            }
+            // if adding the guess fills the WordState array, set State to Won and stop the game loop
+            if (!WordState.Contains('*'))
+            {
+                State = HangmanState.Won;
+                shouldContinue = false;
+            }
+            return shouldContinue;
         }
     }
 }
--- Visualizer.cs
+++ Visualizer.cs
@@ -2,114 +2,113 @@ using System.IO.Compression;
 using System.Security.Cryptography;
 using Hangman.State;
 
-namespace Hangman.Display
+namespace Hangman.Display;
+
+class Visualizer
 {
-    class Visualizer
+    private GameState GameState { get; set; }
+
+    public Visualizer(GameState gameState)
     {
-        private GameState GameState { get; set; }
+        GameState = gameState;
+    }
 
-        public Visualizer(GameState gameState)
+    public void Update()
+    {
+        if (GameState.State is HangmanState.InProgress)
         {
-            GameState = gameState;
-        }
-
-        public void Update()
-        {
-            if (GameState.State is HangmanState.InProgress)
+            Console.Clear();
+            DrawHangman(GameState.GetNumGuesses());
+            DrawWordStateAndGuesses();
+            int guessesLeft = GameState.Guesses.Count(c => c == '\0');
+            Console.WriteLine($"Guesses left: {guessesLeft}");
+            switch (GameState.LastGuessState)
             {
-                Console.Clear();
-                DrawHangman(GameState.GetNumGuesses());
-                DrawWordStateAndGuesses();
-                int guessesLeft = GameState.Guesses.Count(c => c == '\0');
-                Console.WriteLine($"Guesses left: {guessesLeft}");
-                switch (GameState.LastGuessState)
-                {
-                    case GuessState.GuessedCorrect:
-                        Console.WriteLine("Nice! The secret word does contain that character!");
-                        break;
-                    case GuessState.GuessedWrong:
-                        Console.WriteLine("The secret word does not contain that character!");
-                        break;
-                    case GuessState.GuessedRepeated:
-                        Console.WriteLine("You already guessed this character. Try again!");
-                        break;
-                }
+                case GuessState.GuessedCorrect:
+                    Console.WriteLine("Nice! The secret word does contain that character!");
+                    break;
+                case GuessState.GuessedWrong:
+                    Console.WriteLine("The secret word does not contain that character!");
+                    break;
+                case GuessState.GuessedRepeated:
+                    Console.WriteLine("You already guessed this character. Try again!");
+                    break;
             }
         }
-        private void DrawHangman(int numFailedGuesses)
+    }
+    private void DrawHangman(int numFailedGuesses)
+    {
+        Console.WriteLine("  +---+");
+        Console.WriteLine("  |   |");
+        switch (numFailedGuesses)
         {
-            Console.WriteLine("  +---+");
-            Console.WriteLine("  |   |");
-            switch (numFailedGuesses)
-            {
-                case 0:
-                    Console.WriteLine("      |\n" +
-                                      "      |\n" +
-                                      "      |"); break;
-                case 1:
-                    Console.WriteLine("  O   |\n" +
-                                      "      |\n" +
-                                      "      |"); break;
-                case 2:
-                    Console.WriteLine("  O   |\n" +
-                                      "  |   |\n" +
-                                      "      |"); break;
-                case 3:
-                    Console.WriteLine("  O   |\n" +
-                                      " /|   |\n" +
-                                      "      |"); break;
-                case 4:
-                    Console.WriteLine("  O   |\n" +
-                                      " /|\\  |\n" +
-                                      "      |"); break;
-                case 5:
-                    Console.WriteLine("  O   |\n" +
-                                      " /|\\  |\n" +
-                                      " /    |"); break;
-                case 6:
-                    Console.WriteLine("  O   |\n" +
-                                      " /|\\  |\n" +
-                                      " / \\  |"); break;
-                default:
-                    break;
-            }
-            Console.WriteLine("      |");
-            Console.WriteLine("=========");
+            case 0:
+                Console.WriteLine("      |\n" +
+                                  "      |\n" +
+                                  "      |"); break;
+            case 1:
+                Console.WriteLine("  O   |\n" +
+                                  "      |\n" +
+                                  "      |"); break;
+            case 2:
+                Console.WriteLine("  O   |\n" +
+                                  "  |   |\n" +
+                                  "      |"); break;
+            case 3:
+                Console.WriteLine("  O   |\n" +
+                                  " /|   |\n" +
+                                  "      |"); break;
+            case 4:
+                Console.WriteLine("  O   |\n" +
+                                  " /|\\  |\n" +
+                                  "      |"); break;
+            case 5:
+                Console.WriteLine("  O   |\n" +
+                                  " /|\\  |\n" +
+                                  " /    |"); break;
+            case 6:
+                Console.WriteLine("  O   |\n" +
+                                  " /|\\  |\n" +
+                                  " / \\  |"); break;
+            default:
+                break;
         }
+        Console.WriteLine("      |");
+        Console.WriteLine("=========");
+    }
 
-        public void DrawFinalState()
+    public void DrawFinalState()
+    {
+        switch (GameState.State)
         {
-            switch (GameState.State)
-            {
-                case HangmanState.Won:
-                    Console.WriteLine("Congratulations! You saved the Hangman!");
-                    break;
-                case HangmanState.OutOfGuesses:
-                    Console.WriteLine("RIP Hangman, you failed!");
-                    break;
-                default:
-                    Console.WriteLine("Something went terribly wrong! Program failed");
-                    break;
-            }
-            Console.WriteLine($"The secret word was: {GameState.SecretWord}");
+            case HangmanState.Won:
+                Console.WriteLine("Congratulations! You saved the Hangman!");
+                break;
+            case HangmanState.OutOfGuesses:
+                Console.WriteLine("RIP Hangman, you failed!");
+                break;
+            default:
+                Console.WriteLine("Something went terribly wrong! Program failed");
+                break;
         }
+        Console.WriteLine($"The secret word was: {GameState.SecretWord}");
+    }
 
-        private void DrawWordStateAndGuesses()
-        {
-            DrawWordState();
-            Console.Write("         ");
-            DrawGuesses();
-        }
+    private void DrawWordStateAndGuesses()
+    {
+        DrawWordState();
+        Console.Write("         ");
+        DrawGuesses();
+    }
 
-        private void DrawWordState()
-        {
-            Console.Write(string.Join("", GameState.WordState));
-        }
+    private void DrawWordState()
+    {
+        Console.Write(string.Join("", GameState.WordState));
+    }
 
-        private void DrawGuesses()
-        {
-            string guessesUsed = string.Join("", GameState.Guesses);
-            Console.WriteLine($"Guesses used: {string.Join("", GameState.Guesses)}");
-        }
+    private void DrawGuesses()
+    {
+        string guessesUsed = string.Join("", GameState.Guesses);
+        Console.WriteLine($"Guesses used: {string.Join("", GameState.Guesses)}");
     }
 }
